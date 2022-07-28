6838e3dacf019f2f741f2fe415c8363cebbe5a50
daniel2440 <daniel.koszyk024@gmail.com>
2022-07-28 18:35:37 +0200

    test azure env

--- go_server_dk/main.go
+++ go_server_dk/main.go
@@ -1,9 +1,14 @@
 package main
 
-import "go_server_dk/databases"
+import (
+	"go_server_dk/databases"
+	"os"
+)
 
 func main() {
 	databases.InitDatabase()
+	zmienna := os.Getenv("TEST_VAR")
+	println(zmienna)
 	e := initRouting()
 	e.Logger.Fatal(e.Start(":8080"))
 }
