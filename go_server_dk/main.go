110f3067d633277c26d0a6d63c2a9d8bf0893d74
daniel2440 <daniel.koszyk024@gmail.com>
2022-07-28 18:47:02 +0200

    +CORS

--- go_server_dk/main.go
+++ go_server_dk/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4/middleware"
 	"go_server_dk/databases"
 	"os"
 )
@@ -10,5 +11,8 @@ func main() {
 	zmienna := os.Getenv("TEST_VAR")
 	println(zmienna)
 	e := initRouting()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"https://shopworking.azurewebsites.net"},
+	}))
 	e.Logger.Fatal(e.Start(":8080"))
 }
