042483d17ce726e6cff0569245c2c1228977cc03
daniel2440 <daniel.koszyk024@gmail.com>
2022-07-28 20:08:58 +0200

    try env vars

--- go_server_dk/main.go
+++ go_server_dk/main.go
@@ -8,12 +8,12 @@ import (
 
 func main() {
 	databases.InitDatabase()
-	println(os.Getenv("TEST_VAR"))
-	println(os.Getenv("TEST_VAR_TEST_VAR"))
-	println(os.Getenv("TEST_test"))
-	println(os.Getenv("APPSETTING_test"))
-	println(os.Getenv("APPSETTING_TEST_VAR"))
-	println(os.Getenv("test"))
+	println("siema 1" + os.Getenv("TEST_VAR"))
+	println("siema 2" + os.Getenv("TEST_VAR_TEST_VAR"))
+	println("siema 3" + os.Getenv("TEST_test"))
+	println("siema 4" + os.Getenv("APPSETTING_test"))
+	println("siema 5" + os.Getenv("APPSETTING_TEST_VAR"))
+	println("siema 6" + os.Getenv("test"))
 	e := initRouting()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"https://shopworking.azurewebsites.net"},
